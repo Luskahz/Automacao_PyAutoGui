6ed91fc724a3d2f166ae6b38870a8285646b19d7
Lucas Alves de Melo Lis <Lucas.l@IMARUILESTE.DOMINIOLOCAL>
2025-11-19 09:14:27 -0300

    [bug] - ajustes

--- formatos_atualizacao/automatico.py
+++ formatos_atualizacao/automatico.py
@@ -3,12 +3,8 @@ from interfaces.interface_barrier import identificar_interface
 from datetime import datetime
 
 def automatico(formato):
-    print("Bases que serão atualizadas:")
-    for rotina in MAPA_ROTINAS:
-        print(rotina)
-    input("Pressione Enter para continuar: ")
     agora = datetime.now()
-    for rotina, dados in MAPA_ROTINAS.items():
+    for dados in MAPA_ROTINAS.items():
         if "metadado" in dados:
             fn = dados["metadado"]
             metadados = fn(agora)
